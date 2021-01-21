e005abe7e27e8c5102b9cdd5709b11677871b704
GulenkoAnastasia <nansi0307@yahoo.com>
2021-01-21 16:04:57 +0300

    Final Task "Brandi", v-8

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var myCarousel = document.querySelector("#carouselExampleCaptions")
 var carousel = new bootstrap.Carousel(myCarousel, {
-  interval: false,
+  interval: 8000,
   wrap: true,
-  // touch: true
+  touch: true
 })
